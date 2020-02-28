412ad86bf1d04f93eb219110eb60bab17fc83469
Allfaton <61531115+Allfaton@users.noreply.github.com>
2020-02-28 11:44:12 +0700

    Update flauto.js

--- flauto.js
+++ flauto.js
@@ -154,9 +154,9 @@ console.log(chalk`
   {bold.cyan
   —————————————————— [INFORMATION] ————————————————————
   [?] {bold.green FLA | Using Location Media Target!}
-  ——————————————————  [THANKS TO AEXINAV]  ————————————————————
-  [✓] CODER BY AEXINAV (https://fb.me/aexinav)
-  [✓] FIXING & TESTING BY AEXINAV
+  ——————————————————  [THANKS TO ALFATON]  ————————————————————
+  [✓] CODER BY ALFATON (https://fb.me/alfaton)
+  [✓] FIXING & TESTING BY ALFATON
   [✓] TOOLS AMAN 1000000% NO LOGGER
   [✓] GROW UP YOUR INSTAGRAM FOLLOWERS NOW!
   —————————————————————————————————————————————————————}
