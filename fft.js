e025c70b75f57519c45ed94bd42debc384245d26
Allfaton <61531115+Allfaton@users.noreply.github.com>
2020-02-28 11:30:45 +0700

    Update fft.js

--- fft.js
+++ fft.js
@@ -205,7 +205,7 @@ console.log(chalk`
   [?] {bold.green FFTauto | Follow & Comment Only!}
   [?] {bold.green Gunakan komen.txt untk komen!}
   ——————————————————  [THANKS TO ALFATON]  ————————————————————
-  [✓] CODER BY AEXINAV(https://fb.me/alfaton)
+  [✓] CODER BY ALFATON(https://fb.me/alfaton)
   [✓] FIXING & TESTING BY ALFATON
   [✓] TOOLS AMAN 1000000% NO LOGGER
   [✓] GROW UP YOUR INSTAGRAM FOLLOWERS NOW!
