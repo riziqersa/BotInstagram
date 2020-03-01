1072be7b60876079de11f265e5eaf3b237ac1d7e
Allfaton <61531115+Allfaton@users.noreply.github.com>
2020-03-01 22:55:23 +0700

    Update cek.js

--- cek.js
+++ cek.js
@@ -134,14 +134,14 @@ console.log(chalk`
   —————————————————— [INFORMATION] ————————————————————
   [?] {bold.green Check Username Account *AUTO!}
   [?] {bold.green Save Result to Log.txt}
-  ——————————————————  [THANKS TO AEXINAV]  ————————————————————
-  [✓] CODER BY AEXINAV (https://fb.me/aexinav)
-  [✓] FIXING & TESTING BY AEXINAV
+  ——————————————————  [THANKS TO ALFATON]  ————————————————————
+  [✓] CODER BY ALFATON (https://fb.me/alfaton)
+  [✓] FIXING & TESTING BY ALFATON
   [✓] TOOLS AMAN 1000000% NO LOGGER
   [✓] GROW UP YOUR INSTAGRAM FOLLOWERS NOW!
   —————————————————————————————————————————————————————}
       `);
-//aexinav
+//alfato.
 inquirer.prompt(User)
 .then(answers => {
   Excute({
