f67e11a67996da8da487152cdd93816893d5f44f
Allfaton <61531115+Allfaton@users.noreply.github.com>
2020-03-01 22:45:11 +0700

    Update cdl.js

--- cdl.js
+++ cdl.js
@@ -217,9 +217,9 @@ console.log(chalk`
   [?] {bold.green Comment, DM & Like}
   [?] {bold.green Gunakan komen.txt untk komen!}
   
-  ——————————————————  [THANKS TO AEXINAV]  ————————————————————
-  [✓] CODER BY AEXINAV (https://fb.me/aexinav)
-  [✓] FIXING & TESTING BY AEXINAV
+  ——————————————————  [THANKS TO ALFATON]  ————————————————————
+  [✓] CODER BY ALFATON (https://fb.me/alfaton)
+  [✓] FIXING & TESTING BY ALFATON
   [✓] TOOLS AMAN 1000000% NO LOGGER
   [✓] GROW UP YOUR INSTAGRAM FOLLOWERS NOW!
   —————————————————————————————————————————————————————
@@ -227,7 +227,7 @@ console.log(chalk`
   1. Input Target/delay Manual (ITTYW)
   —————————————————————————————————————————————————————}
       `);
-//aexinav
+//alfaton
 inquirer.prompt(User)
 .then(answers => {
   Excute({
